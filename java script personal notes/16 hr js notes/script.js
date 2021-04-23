e42be0e4671e007ded0dadf24be1fa174acb0050
AshutoshMoharana <ashutosh.moharana@yahoo.com>
2021-04-23 23:55:11 +0530

    javascript notes added

---
+++ java script personal notes/16 hr js notes/script.js
@@ -0,0 +1,67 @@
+// console.log('Hello world printing "hello world using ctrl+` and node file name');
+
+// 1.values and variables  in javascript :-----------
+
+// var myName = "Ashtosh Moharana";
+// var myAge = 26;
+// console.log(myName);
+// console.log(myAge);
+// var _myName = "video";
+// var _1my__name = 'vidwdlkjfdj';
+// var 1myName = 'heroAlom'; "this is invalid token due to starting number"
+// var $myName = 'thapatechnical';
+// console.log($myName);
+
+
+// 2.Data types in javascript :----------------------
+
+// 1.checking string type ; 
+// var myName = 'Ashutosh Moharana';
+// console.log(myName);
+
+// 2.boolean operator
+// var imashutosh = true;
+// var imashutosh = false;
+// console.log(typeof(imashutosh));
+// console.log(typeof(imashutosh));
+
+// 3.checking undefined datatype:
+// console.log(typeof(heroallom));
+
+// console.log(10+'20');
+// console.log(9-"5");
+// console.log('java'+'script');
+// console.log(" "+" ");
+// console.log("  "+ 0);
+// console.log('vinod'-'thapa');
+// console.log(true+true);
+// console.log(false+true);
+// console.log(false+true);
+// console.log(false-true);
+
+
+// difference between null vs undefined?
+// Ans:.null means no value (blank object) and undefined means variable with no assign value(and its typeof is undefined).ex:
+
+// var heroallom = null;
+// var imstandby;
+// console.log(imstandby);
+// console.log(typeof(null));
+
+
+// 2.(qn) what is NaN?
+// Ans:NaN is a  property of the global object.
+// in other words , it is a variable in global scope . 
+// The initial value of NaN is Not-A-Number.
+// example:
+// var myphonenumber = 8247374;
+// var myname = "Ashutosh";
+
+// console.log(isNaN(myphonenumber));
+// console.log(isNaN(myname));
+
+// if(isNaN(myname)){
+//     console.log(('please enter a valid phone number'));
+// }
+
+
