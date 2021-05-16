49f371390e21980cdaa05369c27022e89bf8de9b
AshutoshMoharana <ashutosh.moharana@yahoo.com>
2021-05-16 20:15:02 +0530

    express files added

--- java script personal notes/Expressnotes/app.js
+++ java script personal notes/Expressnotes/app.js
@@ -1,14 +1,23 @@
 const express = require('express')
-
-
 const app = express();
 const port = 80;
 
+app.use('/static', express.static('static'))
+
 app.get('/', (req, res)=>{
+    res.status(400).send('this is my first express app with hello')
+
+});
+app.get('/about', (req, res)=>{
     res.send('this is my first express app with hello')
 
-})
+});
+app.post('/about', (req, res)=>{
+    res.send('this is my first post request about express app with hello')
 
+});
 app.listen(port, ()=>{
     console.log(`the application started successfully on port ${port}`);
-})
+});
+
+
